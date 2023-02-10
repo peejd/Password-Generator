7525df64445f97835f3236f0baf11513af0935b5
peejd <121059860+peejd@users.noreply.github.com>
2023-02-10 15:07:43 -0500

    added checkboxes for character types and number input for char length

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charLength = document.getElementById("charLength");
+var inclLowerEl = document.getElementById("inclLower"); 
+var form = document.getElementById("pwGenForm");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+form.addEventListener("submit",  e => {
+  e.preventDefault()
+  var charAmt = charLength.value;
+  var inclLower = inclLowerEl.checked;
+  var password = generatePassword(length, inclLower, inclUpper, inclNumber, InclSpecial);
+})
+// function writePassword() {
+//   var password = generatePassword(length, inclLower, inclUpper, inclNumber, InclSpecial);
+//   var passwordText = document.querySelector("#password");
 
-}
+//   passwordText.value = password;
+
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

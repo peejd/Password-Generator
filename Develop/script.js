b7fdcb04d4468080e0e4ae6cc0ff4c405c58cfaa
peejd <121059860+peejd@users.noreply.github.com>
2023-02-12 16:57:40 -0500

    Added user input elements for character types and password length as well as invalid entry error message element. Added comments to HTML.

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var charLength = document.getElementById("charLength");
-var inclLowerEl = document.getElementById("inclLower"); 
-var form = document.getElementById("pwGenForm");
 
 // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-form.addEventListener("submit",  e => {
-  e.preventDefault()
-  var charAmt = charLength.value;
-  var inclLower = inclLowerEl.checked;
-  var password = generatePassword(length, inclLower, inclUpper, inclNumber, InclSpecial);
-})
-// function writePassword() {
-//   var password = generatePassword(length, inclLower, inclUpper, inclNumber, InclSpecial);
-//   var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 
-//   passwordText.value = password;
-
-// }
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
